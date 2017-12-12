f2ddac1769e04d4a8695b1875ab36536f674dd74
Annie <toussaint.am@gmail.com>
2017-12-11 23:17:49 -0500

    lost day of work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
+window.onload = () => {
+
+    //get navigating buttons & add event listeners to navigating buttons 
+    document.querySelectorAll(".prev").forEach((buttonObj) => {
+        console.log("foreach registered");
+        buttonObj.addEventListener("click", prevFunction, false);
+    });
+
+
+    document.querySelectorAll(".next").forEach((buttonObj) => {
+        console.log("foreach registered");
+        buttonObj.addEventListener("click", nextFunction, false);
+    });
+
+    //get carousel slides, set variable to first slide of set 
+    let slides = document.querySelectorAll(".main ul li");
+       initialSlide = slides[0];
+}
+
+
 //get carousel 
+let carousel = document.querySelector(".main");
 
 //get navigating buttons 
+
+
+let prevFunction = () => {
+    alert("test");
+}
+
+let nextFunction = () => {
+    alert("next test");
+    let slides = document.querySelectorAll(".main ul li");
+   console.log(slides.length)
+}
+
+
+// Array.prototype.push() adds element to end of array 
+// Array.prototype.shift() removes first element in array
 
 //find way to know which slide of the carousel is in view
 
