d47efc837c60d60743bc206f113e514fdbcff486
Toussaint, Anne-Michelle <AToussaint@victoria.com>
2017-12-14 16:55:50 -0500

    carousel working

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@ window.onload = () => {
     //get navigating buttons & add event listeners to navigating buttons 
     document.querySelector(".prev").addEventListener("click", prevFunction, false);
 
-
     document.querySelector(".next").addEventListener("click", nextFunction, false);
-
-    //get carousel slides, set variable to first slide of set 
-    // let slides = document.querySelectorAll(".main ul li");
-    //    initialSlide = slides[0];
-    //    console.log(slides.length)
 }
 
 //get navigating buttons 
@@ -18,36 +12,37 @@ let movingSlide = {
     numberOfSlides: 3,
 }
 
-// let test = document.querySelectorAll(".main ul li");
-//     console.log(test.length);
+// let nxtBtn = document.querySelector(".next");
+// 
+// let prevBtn = document.querySelector(".prev");
+
+// if (movingSlide.currentVisibleSlide >= 2) {
+//     prevBtn.removeAttribute("noShow");
+// }
+    
 console.log(movingSlide.currentVisibleSlide)
 let nextFunction = () => {
 
-    if (movingSlide.currentVisibleSlide < 3) {
+    if (movingSlide.currentVisibleSlide < movingSlide.numberOfSlides) {
       document.querySelector("ul").style.left =  "-" + movingSlide.currentVisibleSlide * movingSlide.widthOfSlide + "px"; 
       movingSlide.currentVisibleSlide++;
       console.log(movingSlide.currentVisibleSlide);
-    } 
-    else if (movingSlide.currentVisibleSlide >= 3) {
-        let nxtBtn = document.querySelector(".next");
+
+        if (movingSlide.currentVisibleSlide === 3) {
+            let nxtBtn = document.querySelector(".next");
             nxtBtn.className = 'noShow';
-    }
+        }
+
+    } 
 }
 
 let prevFunction = () => {
-    
-    document.querySelector("ul").style.left += movingSlide.widthOfSlide + "px"; 
-    movingSlide.currentVisibleSlide--;
-    console.log(movingSlide.currentVisibleSlide);
+    if (movingSlide.currentVisibleSlide > 1 ) {
+        movingSlide.currentVisibleSlide--;
+
+        console.log(movingSlide.currentVisibleSlide);
+        let test = document.querySelector("ul").style.left;
+            test = parseInt(test, 0);
+        document.querySelector("ul").style.left = test + movingSlide.widthOfSlide + "px";  
+    } 
 }
-
-
-// - slides that is visible
-// - width of the slide
-// - how many slides there are
-
-//FINAL QUESTION: How do I move my UL to the left in increments of 300 pixels depending on which slide number I'm on to a maximum allowed by the number of slides I have.
-
-//find way to know which slide of the carousel is in view
-
-//change class based on which slide should be in view and not in view
