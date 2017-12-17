882324f61ab47d83a0bd736b2fee6322465ddf6c
Annie <toussaint.am@gmail.com>
2017-12-17 17:37:06 -0500

    adding method

--- js/main.js
+++ js/main.js
@@ -1,56 +1,52 @@
 window.onload = () => {
     //get navigating buttons & add event listeners to navigating buttons 
-    document.querySelector(".prev").addEventListener("click", prevFunction, false);
+    document.querySelector(".prev").addEventListener("click", movingSlide.prevFunction, false);
 
-    document.querySelector(".next").addEventListener("click", nextFunction, false);
+    document.querySelector(".next").addEventListener("click", movingSlide.nextFunction, false);
 }
 
-//get navigating buttons 
 let movingSlide = {
     currentVisibleSlide: 1,
     widthOfSlide: 300,
     numberOfSlides: 3,
-}
+    nextFunction: () => {
+
+        if (movingSlide.currentVisibleSlide < movingSlide.numberOfSlides) {
+          document.querySelector("ul").style.left =  "-" + movingSlide.currentVisibleSlide * movingSlide.widthOfSlide + "px"; 
+          movingSlide.currentVisibleSlide++;
+          console.log(movingSlide.currentVisibleSlide);
     
-console.log(movingSlide.currentVisibleSlide)
-let nextFunction = () => {
-
-    if (movingSlide.currentVisibleSlide < movingSlide.numberOfSlides) {
-      document.querySelector("ul").style.left =  "-" + movingSlide.currentVisibleSlide * movingSlide.widthOfSlide + "px"; 
-      movingSlide.currentVisibleSlide++;
-      console.log(movingSlide.currentVisibleSlide);
-
+            if (movingSlide.currentVisibleSlide > 1 ){
+               let prevBtn = document.querySelector(".prev");
+               prevBtn.classList.remove("noShow");
+            }
+            
+            if (movingSlide.currentVisibleSlide >= 3) {
+                let nxtBtn = document.querySelector(".next");
+                nxtBtn.className += " noShow";
+            }
+    
+        } 
+    },
+    prevFunction: () => {
+        if (movingSlide.currentVisibleSlide > 1 ) {
+            movingSlide.currentVisibleSlide--;
+    
+            console.log(movingSlide.currentVisibleSlide);
+            let test = document.querySelector("ul").style.left;
+                test = parseInt(test, 0);
+            document.querySelector("ul").style.left = test + movingSlide.widthOfSlide + "px";  
+        } 
+    
         if (movingSlide.currentVisibleSlide > 1 ){
-           let prevBtn = document.querySelector(".prev");
-           prevBtn.classList.remove("noShow");
-        }
-        
-        if (movingSlide.currentVisibleSlide >= 3) {
-            let nxtBtn = document.querySelector(".next");
-            nxtBtn.className += " noShow";
-        }
-
-    } 
-}
-
-let prevFunction = () => {
-    if (movingSlide.currentVisibleSlide > 1 ) {
-        movingSlide.currentVisibleSlide--;
-
-        console.log(movingSlide.currentVisibleSlide);
-        let test = document.querySelector("ul").style.left;
-            test = parseInt(test, 0);
-        document.querySelector("ul").style.left = test + movingSlide.widthOfSlide + "px";  
-    } 
-
-    if (movingSlide.currentVisibleSlide > 1 ){
-        let nextBtn = document.querySelector(".next");
-        nextBtn.classList.remove("noShow");
-        console.log(movingSlide.currentVisibleSlide);
-     }
-
-     if (movingSlide.currentVisibleSlide === 1 ){
-        let prevBtn = document.querySelector(".prev");
-        prevBtn.className += " noShow";
-     }
+            let nextBtn = document.querySelector(".next");
+            nextBtn.classList.remove("noShow");
+            console.log(movingSlide.currentVisibleSlide);
+         }
+    
+         if (movingSlide.currentVisibleSlide === 1 ){
+            let prevBtn = document.querySelector(".prev");
+            prevBtn.className += " noShow";
+         }
+    }
 }
