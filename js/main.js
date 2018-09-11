dcd7889b19a022bbb14fb0d881090ba8a94a70e2
Annie <toussaint.am@gmail.com>
2018-09-10 22:38:03 -0400

    updating to commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-let prevBtn = document.querySelector(".prev"),
-    nxtBtn = document.querySelector(".next");
-
-
 window.onload = () => {
-    //get navigating buttons & add event listeners to navigating buttons 
-    document.querySelector(".prev").addEventListener("click", movingSlide.prevFunction, false);
-
-    document.querySelector(".next").addEventListener("click", movingSlide.nextFunction, false);
-
-    movingSlide.moveThrough();
+    document.querySelectorAll(".nav div").forEach(function(element) {
+        element.addEventListener("click", movingSlide.selectSlide)
+    })
 }
-
-let bgColor = ["#52FF33", "#9933FF", "#FEFE30", "#30EEFE"];
-let bgImage = ['url(https://baconmockup.com/300/300/)', 'url(http://purrfectpals.org/wp-content/uploads/2014/03/DSC_85581-e1404783195381-300x300.jpg)', 'url(http://images.meredith.com/content/dam/bhg/Images/2013/2/4/295212_3839391670148_668524905_n.jpg.rendition.largest.ss.jpg)'];
 
 function addAnimiation(element) {
     element.classList.add("animation")
@@ -20,66 +10,25 @@ function addAnimiation(element) {
 
 let movingSlide = {
     currentVisibleSlide: 1,
-    widthOfSlide: 300,
-    numberOfSlides: document.querySelectorAll("ul li").length,
-    moveThrough: () => { 
-        document.querySelectorAll("ul li").forEach((slide, slideIndex ) => { 
-            console.log(slideIndex);
-            console.log(slide);
-            slide.style.background = bgColor[slideIndex%bgColor.length]; 
-        })
-    },
-    nextFunction: () => {
-        
-        if (movingSlide.currentVisibleSlide <  movingSlide.numberOfSlides) {
+    widthOfSlide: 500,
+    currentPosition: 1,
+    numberOfSlides: document.querySelectorAll(".main .carousel ul div"),
+    selectSlide: () => {
+        var navSlideValue = event.target.dataset.value;
 
-            var test = document.querySelector("ul");
-            var test2 = document.querySelector("ul li");
+        for(i = 0; i < movingSlide.numberOfSlides.length; i++) {
+            if(navSlideValue === movingSlide.numberOfSlides[i].dataset.value) {
+                var test = document.querySelector("ul");
+                test.style.transform = "translateX(-" + movingSlide.widthOfSlide + "px)";
 
-            test.style.transform = "translateX(-" + movingSlide.currentVisibleSlide * movingSlide.widthOfSlide + "px)";
-            addAnimiation(test);
-            test.addEventListener('animationEvent', function(event) { this.classList = ''}, false);
-
-            document.querySelector("ul").animate([
-                // keyframes
-                { transform: 'translateX(' + movingSlide.currentVisibleSlide * movingSlide.widthOfSlide + 'px)' }, 
-                { transform: 'translateX(-' + movingSlide.currentVisibleSlide * movingSlide.widthOfSlide + 'px)' }
-                
-              ], { 
-                // timing options
-                duration: 1000
-              });;
-            movingSlide.currentVisibleSlide++;
-            console.log(movingSlide.currentVisibleSlide);
-    
-            if (movingSlide.currentVisibleSlide > 1 ){
-               prevBtn.classList.remove("noShow");
+                // movingSlide.currentPosition++;
+                // console.log(movingSlide.currentPosition);
+                movingSlide.currentPosition = test.style.transform;
+                console.log(test.style.transform);
+                // addAnimiation(test);
+                test.addEventListener('animationEvent', function(event) { this.classList = ''}, false);
             }
-            
-            if (movingSlide.currentVisibleSlide >= movingSlide.numberOfSlides) {
-                nxtBtn.className += " noShow";
-            }
-            
-    
-        } 
-    },
-    prevFunction: () => {
-        if (movingSlide.currentVisibleSlide > 1 ) {
-            movingSlide.currentVisibleSlide--;
-    
-            console.log(movingSlide.currentVisibleSlide);
-            let test = document.querySelector("ul").style.left;
-                test = parseInt(test, 0);
-            document.querySelector("ul").style.left = test + movingSlide.widthOfSlide + "px";  
-        } 
-    
-        if (movingSlide.currentVisibleSlide > 1 ){
-            nxtBtn.classList.remove("noShow");
-            console.log(movingSlide.currentVisibleSlide);
-         }
-    
-         if (movingSlide.currentVisibleSlide === 1 ){
-            prevBtn.className += " noShow";
-         }
+            movingSlide.currentVisibleSlide = navSlideValue;
+        }
     }
 }
